2cda26494a905c4a2cfec800988171abfe18ebd4
Krysiah <Krysiah.Travis@laetottenham.ac.uk>
2024-11-25 18:57:37 +0000

    Task 3 done!

--- main.py
+++ main.py
@@ -37,10 +37,35 @@ class Student(Person):
     
     def get_student_summary(self):
         print("Student ID:", self.student_id, "¦", "Course:", self.course,"¦", "Grades:", self.calcuate_avg_grade())
-    
-#Kryssi = Person("Krysia", 16, "Male")
-#Kryssi.set_details("Krysiah", 17, "Female")
-#Kryssi.get_details()
+
+class Professor(Person):
+    def __init__(self, staff_id, department, salary):
+        self.staff_id = staff_id
+        self.department = department
+        self.salary = salary
+
+    def set_professor_details(self,staff_id, department, salary):
+        self.staff_id = staff_id
+        self.department = department
+        self.salary = salary
+
+    def give_feedback(self, student, feedback):
+        print("Feedback for", student,":",feedback)
+
+    def increase_salary(self):
+        twenty_pct = self.salary + ((self.salary/10)*2)
+        return twenty_pct
+
+    def get_professor_summary(self):
+        print("Name:", self.name, "¦", "Age:", self.age,"¦", "Gender:", self.gender)
+        print("Staff ID:", self.staff_id, "¦", "Department:", self.department,"¦", "Salary:", self.increase_salary())
+
+
+#------------------------------------------------------------------------------------------
+
+Kryssi = Person("Krysia", 16, "Male")
+Kryssi.set_details("Krysiah", 17, "Female")
+Kryssi.get_details()
    
 Kryssi = Student(9482, "Computing")
 Kryssi.set_student_details(9482, "Computing", 0)
@@ -53,7 +78,12 @@ Kryssi.add_grade(97)
 
 Kryssi.get_student_summary()
 
+Miss_T = Professor("Miss Travis",21,"Female")
+Miss_T.set_details("Miss Travis",21,"Female")
+Miss_T.set_professor_details(1045,"Computing",100000)
+#Miss_T.give_feedback("Kryssi","Splendid coding work!")
 
+Miss_T.get_professor_summary()
 
 
 
